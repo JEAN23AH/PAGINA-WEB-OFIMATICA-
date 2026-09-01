c1aafdd356ee56eadd51974c994317489fb84a9e
Jean de la cruz <rgbrecta@gmail.com>
2026-08-31 21:30:20 -0500

    actualizaciones de: QR,ramas html

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Repositorio Tecnologico iniciado");
+         "actualizacion de QR"
+         "actualizacion de rama principal"
+         "actualizacion de rama actualizaciones, programas, scripts, style, tutoriales, videotutoriales, configuracion, index, y otros  archivos"
+         
